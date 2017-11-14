5102231e181bd4d8e498a27b1ae40a57e7592cda
Jorge Fernández de Cossío Díaz <j.cossio.diaz@gmail.com>
2017-11-14 14:17:29 +0000

    sumt

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,5 +1,14 @@
 module Utils
 
-# package code goes here
+export sumt
+
+
+"safe typed sum. Returns correct zero of correct type when iterator is empty."
+function sumt(::Type{T}, itr)::T where T
+    isempty(itr) ? zero(T) : sum(x::T for x in itr)::T
+end
+
+
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Utils
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+
+@test sumt(Int, i for i = 1:10 if i < 0) == 0
+@test sumt(Int, i for i = 1:10 if i < 0) isa Int
